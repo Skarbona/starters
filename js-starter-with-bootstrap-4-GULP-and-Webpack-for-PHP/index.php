531c0e86ea94211445ba79c9b6ba9c3201064ed1
Skarbona <filip.sokolowski1990@gmail.com>
2018-03-27 15:15:30 +0200

    add PHP support
    with gulp and webpack

---
+++ js-starter-with-bootstrap-4-GULP-and-Webpack-for-PHP/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>JS Starter Pack with Bootstrap</title>
+  <link rel="stylesheet" href="view/css/font-awesome.min.css">
+  <link rel="stylesheet" href="view/css/bootstrap.css">
+  <link rel="stylesheet" href="view/css/style.css">
+</head>
+<body>
+  <h1>JS Starter Pack with Bootstrap</h1>
+  <p>Version 4.0.0-beta</p>
+
+  <script src="view/js/jquery.min.js"></script>
+  <script src="view/js/popper.min.js"></script>
+  <script src="view/js/bootstrap.min.js"></script>
+  <script src="view/js/bundle.js"></script>
+</body>
+</html>
